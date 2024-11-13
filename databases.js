13a23ad12d6f7e4c2cf27add82252112be5c89cc
Kunal Khallar <khallark295@gmail.com>
2024-11-13 12:47:18 +0530

    modified:   databases.js

--- databases.js
+++ databases.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 export async function addProduct(category, product_name, batch_number, qty, price, manufacturer_name, bill_number, expiry_date, is_sold) {
     try {
         await pool.query(`
-            INSERT INTO products (category, product_name, batch_number, qty, price, manufacturer_name, bill_number, expiry_date)
+            INSERT INTO products (category, product_name, batch_number, qty, price, manufacturer_name, bill_number, expiry_date, is_sold)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);
         `, [category, product_name.replace(/\s+/g, ' ').trim(), batch_number, qty, price, manufacturer_name.replace(/\s+/g, ' ').trim(), bill_number, expiry_date, is_sold]);
     } catch (error) {
